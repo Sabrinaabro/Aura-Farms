77193490e5a03831561d61e10db4181ba8f65caf
Sabrinaabro <sabrinaazam123@gmail.com>
2024-05-19 00:53:38 +0500

    main branch

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Info from "./Info/Info";
+import Story from "./Story/Story";
+import Products from "./Products/Products";
+import Footer from "./Footer/Footer";
+
+export { Header, Info, Story, Products, Footer };
+
