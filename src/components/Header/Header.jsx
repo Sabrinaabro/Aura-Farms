a6b91b3ccfbc66ae9079ea6919c5a7d5ea7818d6
Sabrinaabro <sabrinaazam123@gmail.com>
2024-05-21 19:13:13 +0500

    Adds header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      
+      <div className='wrapper'>
+        <div className='cta'>
+        <p className='name'>Improve Your Crops With</p>
+        <h1>Aura Farms</h1>
+        <a href='#' className='btn'>
+          Our Products
+        </a>
+        </div>
+      </div>
     </header>
   )
 }
