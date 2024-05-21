f6dd99de576e3e92a82128fcdb29647d276bfd96
Sabrinaabro <sabrinaazam123@gmail.com>
2024-05-21 23:00:05 +0500

    adds navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./Header.css";
-import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
     <header>
       <div className='wrapper'>
-        <Navbar />
         <div className='cta'>
         <p className='name'>Improve Your Crops With</p>
         <h1>Aura Farms</h1>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,38 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div className='logo'>
+        <img src="src/assets/icon.png" />
+      </div>
+
+      <a href='#' className='search'>
+        <img src="src/assets/search.svg" />
+        </a>
+
+        <a href='#' className='btn'>
+          Contact Us
+        </a>  
+
+      <ul>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+
+        <li>
+          <a href='#'>Services</a>
+        </li>
+
+        <li>
+          <a href='#'>Shop</a>
+        </li>
+
+        <li>
+          <a href='#'>How It Works</a>
+        </li>
+
+      </ul>
+    </nav>
   )
 }
 
