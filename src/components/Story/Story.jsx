2b0c6ed599a76d9ccd26b8541fc378ce88501caa
Sabrinaabro <sabrinaazam123@gmail.com>
2024-05-22 20:11:19 +0500

    adds story section

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -3,7 +3,23 @@ import "./Story.css";
 
 const Story = () => {
   return(
-    <div>Story</div>
+    <story>
+    <div className='storywrapper'>
+    <h1>Our Stories Cultivate Excellence</h1>
+    <div className='card'>
+      <img src='src/assets/Card.png'></img>
+    </div>
+    <div className='card1'>
+      <img src='src/assets/Card1.png'></img>
+    </div>
+    <div className='card2'>
+      <img src='src/assets/Card2.png'></img>
+    </div>
+    <div className='card3'>
+      <img src='src/assets/Card3.png'></img>
+    </div>
+    </div>
+    </story>
   )
 }
 
